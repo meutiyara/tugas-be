0cee52ccdb8596fa8311a3e00ca33b2f4e52f585
meutiyara <553241106@student.smktelkom-lpg.sch.id>
2025-08-29 15:10:03 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NAIPAMEW</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+    <h1>KELAS XI RPL 2</h1>
+    <nav>
+        <a href="index.php">Home</a> |
+            <a href="about.php">About</a> |
+            <a href="contact.php">Contact</a>
+        </nav>
+    </header>
+    <main>
+</body>
+</html>
