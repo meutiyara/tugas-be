39b86a261c9738a08b5ccea7ddffd023edbee683
meutiyara <553241106@student.smktelkom-lpg.sch.id>
2025-08-29 15:13:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+?>
+<h2>Selamat Datang di Website naipamew</h2>
+<p>Ini adalah halaman utama (Home). Website ini dibuat untuk belajar PHP dasar dengan include, require, dan CSS eksternal.</p>
+<?php
+require 'footer.php';
+?>
