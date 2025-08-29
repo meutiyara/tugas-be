5756b79a3ccdfa870877c923dc83f568141c771b
meutiyara <553241106@student.smktelkom-lpg.sch.id>
2025-08-29 15:07:32 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+?>
+<h2>Tentang Website</h2>
+<p>Website ini dibuat untuk latihan dasar PHP, termasuk function, scope, serta include dan require.</p>
+<?php
+require 'footer.php';
+?>
